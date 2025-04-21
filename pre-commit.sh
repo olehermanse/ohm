2873f2dfca54a52a50a148855094bfc23a89c053
Ole Herman Schumacher Elgesem <ole.elgesem@northern.tech>
2025-04-21 16:13:06 +0200

    Removed pre-commit hook
    Signed-off-by: Ole Herman Schumacher Elgesem <ole.elgesem@northern.tech>

--- pre-commit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# Automatically check formatting before commiting, to enable:
-#   cp -a pre-commit.sh .git/hooks/pre-commit
-
-prettier --check *.html || (echo "Run: prettier --write *.html" ; exit 1)
